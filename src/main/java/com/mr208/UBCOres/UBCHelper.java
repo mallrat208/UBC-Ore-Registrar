d1db04bf2479715f9b4ccd4657cb5c73ed082fa9
mallrat208 <marwat208@gmail.com>
2015-03-01 00:56:46 -0500

    Cleaning up UBCHelper
    Removing Duplicate Code, no reason to have twice the work.

--- src/main/java/com/mr208/UBCOres/UBCHelper.java
+++ src/main/java/com/mr208/UBCOres/UBCHelper.java
@@ -9,24 +9,10 @@ import net.minecraft.item.ItemStack;
 public class UBCHelper {
     public static void registerOreBlock(Block block, int meta,String name,FMLPreInitializationEvent event) {
 
-        ItemStack placeholder = new ItemStack(block,1,meta);
-
-
-
-        try {
-            UBAPIHook.ubAPIHook.ubOreTexturizer.setupUBOre(block,
-                                                            meta,
-                                                            "ubcores:" + name + "_overlay",
-                                                            "ore." + name,
-                                                            event);
-        } catch (Exception e) {
-            if (e instanceof UBOreTexturizer.BlocksAreAlreadySet)
-                Log.error(placeholder.getDisplayName() + " is already registered with UBC. Skipping");
-            placeholder = null;
-        }
-        if(placeholder!=null) Log.info("Added " + placeholder.getDisplayName() + " for Underground Biomes Ore Texturization");
+        registerOreBlock(block,meta,"ubcores",name + "_overlay",name,event);
     }
-    public static void registerOreBlockWithModTexture(Block block, int meta,String modname, String texturename, String name,FMLPreInitializationEvent event) {
+
+    public static void registerOreBlock(Block block, int meta,String modname, String texturename, String name,FMLPreInitializationEvent event) {
 
         ItemStack placeholder = new ItemStack(block,1,meta);
         try {
