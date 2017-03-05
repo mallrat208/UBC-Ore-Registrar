381cc4787d8f0b95213bec5b4b1e764b456c0c22
Andrew W <marwat208@gmail.com>
2017-03-05 18:20:48 -0500

    More Mod Support
    Added Support for the folllowing Mods:
    Embers
    Ars Magica 2
    Magneticraft
    Draconic Evolution
    Mystical Agriculture
    Railcraft
    
    Textures to follow

--- src/main/java/com/mr208/UBCOres/Mods.java
+++ src/main/java/com/mr208/UBCOres/Mods.java
@@ -41,5 +41,41 @@ public class Mods {
         UBCHelper.registerModOresWithMeta("bigreactors","brore",0,1,ORES);
     }
 
+    public static void Embers() {
+        Ores[] ORES = {Ores.COPPER,Ores.LEAD,Ores.SILVER};
+        String[] BLOCKNAMES = {"oreCopper","oreLead","oreSilver"};
+        UBCHelper.registerModOresWithoutMeta("embers", BLOCKNAMES, ORES);
+    }
+
+    public static void ArsMagica2() {
+        Ores[] ORES = {Ores.VINTEUM,Ores.CHIMERITE,Ores.BLUE_TOPAZ,Ores.MOONSTONE};
+        UBCHelper.registerModOresWithMeta("arsmagica2","ore",0,1,ORES);
+    }
+
+    public static void Magneticraft() {
+        Ores[] ORES = {Ores.COPPER,Ores.LEAD,Ores.COBALT,Ores.TUNGSTEN};
+        UBCHelper.registerModOresWithMeta("magneticraft","ore_block",0,1,ORES);
+    }
+
+    public static void DraconicEvolution() {
+        Ores[] ORES = {Ores.DRACONIUM};
+        UBCHelper.registerModOresWithMeta("draconicevolution","draconium_ore",0,1,ORES);
+    }
+
+    public static void MysticalAgriculture() {
+        Ores[] ORES = {Ores.PROSPERITY,Ores.INFERIUM};
+        String[] BLOCKNAMES = {"prosperity_ore","inferium_ore"};
+        UBCHelper.registerModOresWithoutMeta("mysticalagriculture",BLOCKNAMES,ORES);
+    }
+
+    public static void Railcraft() {
+        Ores[] ORES = {Ores.COPPER,Ores.TIN,Ores.LEAD,Ores.SILVER};
+        Ores[] ORES_POOR = {Ores.POOR_IRON, Ores.POOR_GOLD, Ores.POOR_COPPER, Ores.POOR_TIN, Ores.POOR_LEAD,Ores.POOR_SILVER};
+        UBCHelper.registerModOresWithMeta("railcraft","ore",11,1,ORES);
+        UBCHelper.registerModOresWithMeta("railcraft","ore",5,1,ORES_POOR);
+    }
+
+
+
 
 }
